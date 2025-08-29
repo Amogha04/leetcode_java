0b8f0787f38bc7830552d0c73ec3e739532011d1
Amogha04 <amoghvarsha7246@gmail.com>
2025-08-29 15:14:40 +0530

    Problem 116

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -122,4 +122,23 @@ public class BinaryTree {
         }
         return result;
     }
+    public Node connect(Node root) {
+    //https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+      if(root == null){
+          return null;
+      }
+      Node leftmost = root;
+      while (leftmost.left != null){
+          Node current = leftmost;
+          while (current != null){
+              current.left.next = current.right;
+              if(current.next != null){
+                  current.right.next = current.next.left;
+              }
+              current = current.next;
+          }
+          leftmost = leftmost.left;
+          }
+      return root;
+    }
 }
