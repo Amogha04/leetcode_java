0903c834f09f576da030af4e913bcaa92b7293e5
Amogha04 <amoghvarsha7246@gmail.com>
2025-08-29 18:49:09 +0530

    Diameter of BT

--- src/Trees/DFS.java
+++ src/Trees/DFS.java
@@ -1,4 +1,35 @@
 package Trees;
 
 public class DFS {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    Node root;
+    int diameter = 0;
+    public int diameterOfBinaryTree(Node root) {
+    //https://leetcode.com/problems/diameter-of-binary-tree/
+      height(root);
+      return diameter - 1;
+    }
+    public int height(Node node){
+        if(node == null){
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int dia = leftHeight + rightHeight + 1;
+        diameter = Math.max(dia,diameter);
+
+        return Math.max(leftHeight,rightHeight) + 1;
+    }
+
 }
