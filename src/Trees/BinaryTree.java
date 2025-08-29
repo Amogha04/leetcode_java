4d669710e6fa1d6cf3633bdf3772baf01fd50770
Amogha04 <amoghvarsha7246@gmail.com>
2025-08-29 18:17:27 +0530

    Are they symmetrical?

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -208,4 +208,18 @@ public class BinaryTree {
         }
         return false;
     }
+
+    public boolean isSymmetric(Node root) {
+        //https://leetcode.com/problems/symmetric-tree/submissions/1752353718/
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(Node t1, Node t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        if (t1.value != t2.value) return false;
+
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
 }
