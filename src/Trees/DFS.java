01e251a4a3ef007b149aea1531582722d1deed72
Amogha04 <amoghvarsha7246@gmail.com>
2025-08-30 16:14:49 +0530

    Construct a BT with given preorder and inorder

--- src/Trees/DFS.java
+++ src/Trees/DFS.java
@@ -148,5 +148,26 @@ public class DFS {
 
         inorder(node.right, k);
     }
+    public Node buildTree(int[] preorder, int[] inorder) {
+     //https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+        if(preorder.length == 0){
+            return null;
+        }
+        int r = preorder[0];
+        int index = 0;
+        for (int i = 0;i < inorder.length;i++){
+            if(inorder[i] == r){
+                index = i;
+            }
+        }
+        Node node = new Node(r);
+
+        node.left = buildTree(Arrays.copyOfRange(preorder,1,index + 1),
+                Arrays.copyOfRange(inorder,0,index));
+        node.right = buildTree(Arrays.copyOfRange(preorder,index,preorder.length),
+                Arrays.copyOfRange(inorder,index,inorder.length));
+
+        return node;
+    }
 }
 
